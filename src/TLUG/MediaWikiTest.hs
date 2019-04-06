35ce294133f11fdbe95cba8d2323a20ecad32dce
Curt J. Sampson <cjs@cynic.net>
2019-04-06 18:36:00 +0900

    FIXUP TLUG.MediaWikiTest: Fix line wrap

--- src/TLUG/MediaWikiTest.hs
+++ src/TLUG/MediaWikiTest.hs
@@ -8,7 +8,8 @@ import TLUG.MediaWiki
 test_parse = do
      assertEqual [Markup "abc"] (parsePage "abc")
      assertEqual [ Markup "hello"
-                 , Transclude "TranscludeName" [("", "value1"), ("",""), ("name2", "value2")]
+                 , Transclude "TranscludeName"
+                     [("", "value1"), ("",""), ("name2", "value2")]
                  , Markup "goodbye"
                  ]
          (parsePage "hello{{TranscludeName|value1||name2=value2}}goodbye")
