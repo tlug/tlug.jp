95569b6bd04514912150c6bad68f556fe8496543
sssjjjnnn <tlug@consalon.com>
2019-04-11 18:09:28 +0900

    WIP: Implement Parser Alternative for many; Clean up redundant tests
    The parser function now returns Maybe so parsers can fail to match
    without erroring out. This is needed for Alternative to work.

--- src/TLUG/MediaWikiTest.hs
+++ src/TLUG/MediaWikiTest.hs
@@ -4,25 +4,10 @@ module TLUG.MediaWikiTest (htf_thisModulesTests) where
 
 import Test.Framework
 import TLUG.MediaWiki
-import Data.Char
 import Control.Applicative
 
-test_runparser = do
-    assertEqual 'x' (runParser char "x")
-
-test_readNothing = do
-    assertEqual 17 (runParser readNothingGiveInt "")
-
-test_parserFmap = do
-    assertEqual 'X' (runParser (fmap toUpper char) "x")
-
-test_parserPure = do
-    assertEqual 'a' (runParser (pure 'a') "")
-
 test_parserApp = do
-    assertEqual ('a','y') (runParser (pure (\a b -> (a,b)) <*> char <*> char) "ay")
-
-test_parserApp2 = do
+    assertEqual ('a','y') (runParser ((\a b -> (a,b)) <$> char <*> char) "ay")
     assertEqual ('a','y') (runParser (liftA2 (\a b -> (a,b)) char char) "ay")
 
 test_parserMonad = do
@@ -33,12 +18,10 @@ test_parserMonad = do
                                      )
                            "ay")
 
-{-
-test_parsePage = do
-    assertEqual [Markup "abc"] (parsePage "abc")
-
 test_parse = do
-     assertEqual [Markup "abc"] (parsePage "abc")
+    assertEqual [] (parsePage "")
+    assertEqual [Markup "abc"] (parsePage "abc")
+{-
      assertEqual [ Markup "hello"
                  , Transclude "TranscludeName"
                      [("", "value1"), ("",""), ("name2", "value2")]
