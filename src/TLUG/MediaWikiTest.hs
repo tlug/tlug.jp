2ebc4509bbb8b484f0fd7c52c26eaf42d5ffaf86
sssjjjnnn <tlug@consalon.com>
2019-04-12 12:17:06 +0900

    WIP: Add some more parser tests

--- src/TLUG/MediaWikiTest.hs
+++ src/TLUG/MediaWikiTest.hs
@@ -21,12 +21,15 @@ test_parserMonad = do
 test_parse = do
     assertEqual [] (parsePage "")
     assertEqual [Markup "abc"] (parsePage "abc")
+    assertEqual [Transclude "trans:a b" []] (parsePage "{{trans:a b}}")
+    assertEqual [Markup "** ", Transclude "w:ja" [(Nothing,"利用者:name")]] (parsePage "** {{w:ja|利用者:name}}")
     assertEqual [
         Markup "begin",
         Transclude "trans" [(Nothing,"value1"), (Nothing,""), (Just "name2","value2")],
+        Transclude "t2" [],
         Markup "end"
         ]
-        (parsePage "begin{{trans|value1||name2=value2}}end")
+        (parsePage "begin{{trans|value1||name2=value2}}{{t2}}end")
     assertEqual [Markup "{{{}}}{{{{}}}}{}"] (parsePage "{{{}}}{{{{}}}}{}")
 {-
     assertEqual [Markup "hello", Transclude "hi" [("", "bye"), ("","")]]
