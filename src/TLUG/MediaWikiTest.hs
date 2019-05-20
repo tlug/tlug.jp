13f77e83da544ada78425409ef1610af0ee5252f
Curt J. Sampson <cjs@cynic.net>
2019-05-20 22:04:25 +0900

    TLUG.MediaWiki: Rename variables and reformat
    Better match standard Haskell conventions. Put functions in more or
    less high-level to low-level order.

--- src/TLUG/MediaWikiTest.hs
+++ src/TLUG/MediaWikiTest.hs
@@ -7,31 +7,47 @@ import TLUG.MediaWiki
 import Control.Applicative
 
 test_parserApp = do
-    assertEqual ('a','y') (runParser ((\a b -> (a,b)) <$> anyChar <*> anyChar) "ay")
-    assertEqual ('a','y') (runParser (liftA2 (\a b -> (a,b)) anyChar anyChar) "ay")
+    assertEqual
+        ('a','y')
+        $ runParser ((\a b -> (a,b)) <$> char <*> char) "ay"
+    assertEqual
+        ('a','y')
+        $ runParser (liftA2 (\a b -> (a,b)) char char) "ay"
 
-test_parserMonad = do
-    assertEqual ('a','y') (runParser (do
-                                             a <- anyChar
-                                             b <- anyChar
-                                             return (a,b)
-                                     )
-                           "ay")
+test_parserMonad =
+    let parser = do a <- char
+                    b <- char
+                    return (a,b)
+    in assertEqual ('a','y') $ runParser parser "ay"
 
 test_parse = do
-    assertEqual [] (parsePage "")
-    assertEqual [Markup "abc" False] (parsePage "abc")
-    assertEqual [Transclude "trans:a b" []] (parsePage "{{trans:a b}}")
-    assertEqual [Markup "** " False, Transclude "w:ja" [(Nothing,"利用者:name")]] (parsePage "** {{w:ja|利用者:name}}")
-    assertEqual [Markup "inc" False, Markup "noinc" True] (parsePage "inc<noinclude>noinc</noinclude>")
-    assertEqual [
-        Markup "begin" False,
-        Transclude "trans" [(Nothing,"value1"), (Nothing,""), (Just "name2","value2")],
-        Transclude "t2" [],
-        Markup "end" True
+    assertEqual
+        []
+        $ parsePage ""
+    assertEqual
+        [Markup "abc" False]
+        $ parsePage "abc"
+    assertEqual
+        [Transclude "trans:a b" []]
+        $ parsePage "{{trans:a b}}"
+    assertEqual
+        [Markup "** " False, Transclude "w:ja" [(Nothing,"利用者:name")]]
+        $ parsePage "** {{w:ja|利用者:name}}"
+    assertEqual
+        [Markup "inc" False, Markup "noinc" True]
+        $ parsePage "inc<noinclude>noinc</noinclude>"
+    assertEqual
+        [ Markup "begin" False
+        , Transclude "trans"
+            [ (Nothing,"value1"), (Nothing,""), (Just "name2","value2") ]
+        , Transclude "t2" []
+        , Markup "end" True
         ]
-        (parsePage "begin{{trans|value1||name2=value2}}{{t2}}<noinclude>end</noinclude>")
-    assertEqual [Markup "{{{}}}{{{{}}}}{}" False] (parsePage "{{{}}}{{{{}}}}{}")
+        $ parsePage  $ "begin{{trans|value1||name2=value2}}"
+                    ++ "{{t2}}<noinclude>end</noinclude>"
+    assertEqual
+        [Markup "{{{}}}{{{{}}}}{}" False]
+        $ parsePage "{{{}}}{{{{}}}}{}"
 
 {- How do we test errors?
     assertEqual [Markup "hello", Transclude "hi" [("", "bye"), ("","")]]
