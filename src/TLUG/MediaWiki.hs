8db555c2d440bfec73142654b8bb59339ad1c0b1
sssjjjnnn <tlug@consalon.com>
2019-06-02 10:48:06 +0900

    TLUG.Parser: Refactor parser core into separate module

--- src/TLUG/MediaWiki.hs
+++ src/TLUG/MediaWiki.hs
@@ -49,56 +49,7 @@ import Control.Applicative;
 import Data.List;
 import Data.Maybe;
 import Text.Read;
-
-----------------------------------------------------------------------
---  Monadic parser framework
-
-data ParserState = ParserState
-    { remaining :: String
-    }
-newtype Parser a = Parser { parse :: (ParserState -> Maybe (a, ParserState)) }
-
-instance Functor Parser where
-    fmap :: (a -> b) -> Parser a -> Parser b
-    fmap f (Parser parse) =
-        Parser $ \state ->
-            case parse state of
-                Nothing -> Nothing
-                Just (x, state') -> Just (f x, state')
-
-instance Applicative Parser where
-    pure :: a -> Parser a
-    pure x = Parser $ \state -> Just (x, state)
-    (<*>) :: Parser (a -> b) -> Parser a -> Parser b
-    (Parser parse1) <*> parser2 =
-        Parser $ \state -> case parse1 state of
-            Nothing -> Nothing
-            Just (f, state') -> parse (fmap f parser2) state'
-
-instance Alternative Parser where
-    empty :: Parser a
-    empty = Parser $ return Nothing
-    (<|>) :: Parser a -> Parser a -> Parser a
-    (Parser parse1) <|> (Parser parse2) =
-        Parser $ \state -> case parse1 state of
-                               Nothing -> parse2 state
-                               x -> x
-
-instance Monad Parser where
-    (>>=) :: Parser a -> (a -> Parser b) -> Parser b
-    parser >>= k =
-        Parser $ \state ->
-            case (parse parser) state of
-                Nothing -> Nothing
-                Just (x, state') -> parse (k x) state'
-
--- | Run a given parser on a string
-runParser :: Parser a -> String -> a
-runParser (Parser f) s =
-    case f (ParserState s) of
-        Nothing                  -> error "Parse failed"
-        Just (x, ParserState "") -> x
-        Just (x, ParserState s)  -> error $ "Incomplete parse: " ++ s
+import TLUG.Parser;
 
 ----------------------------------------------------------------------
 --  Parser for transcludes and other text from MediaWiki markup.
@@ -146,6 +97,8 @@ strPP a b = b { body = a ++ body b }
 concatPP :: ProcPage -> ProcPage -> ProcPage
 concatPP a = strPP $ body a
 
+-- | Replace positional/named parameter references in a transcluded
+-- | file (e.g. {{{param}}}) with the passed-in values.
 replaceParam :: [Param] -> String -> String
 replaceParam params name =
     let namepar = find (\(pn,pv) -> pn == Just name) params
@@ -234,41 +187,3 @@ numBraces f = Parser $ \state ->
         if f count
             then Just (take count rem, ParserState $ drop count rem)
             else Nothing
-
-isAnyPrefixOf [] s      = False
-isAnyPrefixOf (p:xp) s  = (p `isPrefixOf` s) || (xp `isAnyPrefixOf` s)
-
--- Check for a match without consuming any input.
-match s = Parser $ \state ->
-    if s `isAnyPrefixOf` (remaining state)
-        then Just (s, state)
-        else Nothing
-
--- Inverts a match parser
-notp (Parser p) = Parser $ \state ->
-    case p state of
-        Nothing -> Just ([], state)
-        otherwise -> Nothing
-
-noMatch s = notp (match s)
-
--- | Parse a single char
-char :: Parser Char
-char = Parser $ \state ->
-    case (remaining state) of
-        ""       -> Nothing
-        (c:cs)   -> Just (c, ParserState cs)
-
--- Parses any char unless looking at one of the specified strings
-anyExcept :: [String] -> Parser Char
-anyExcept s = Parser $ \state ->
-    if s `isAnyPrefixOf` (remaining state)
-        then Nothing
-        else parse char state
-
--- Parses any fixed string
-string :: String -> Parser String
-string t = Parser $ \state ->
-    case stripPrefix t (remaining state) of
-        Just xs -> Just (t, ParserState xs)
-        Nothing -> Nothing
