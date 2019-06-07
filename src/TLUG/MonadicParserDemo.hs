c5d83c71504daa35ee76fb1c8cfa7af58b8cfc8e
Curt J. Sampson <cjs@cynic.net>
2019-06-07 23:34:35 +0900

    TLUG.MonadicParserDemo: Give this a good edit
    Change "give back" to "produce," among many other things.
    
    I needed to re-read this carefully anyway after being a long time
    away, so I reckoned I might as well do a cleanup at the same time.

--- src/TLUG/MonadicParserDemo.hs
+++ src/TLUG/MonadicParserDemo.hs
@@ -16,9 +16,10 @@
 -- test_* functions below into tests that get run.
 {-# OPTIONS_GHC -F -pgmF htfpp #-}
 
--- This allows us to use type signatures on the functions we define
--- in @instance@ declarations that supply the functions to make a
--- type a member of a class.
+-- This allows us to use type signatures on the functions we define in
+-- @instance@ declarations. The instance declarations make a type a a
+-- type a member of a class by supplying the functions must be
+-- available for every member of that class.
 {-# LANGUAGE InstanceSigs #-}
 
 module TLUG.MonadicParserDemo (htf_thisModulesTests) where
@@ -26,15 +27,15 @@ import Test.Framework
 
 
 {-  |
-    During the parse we need to maintain some state, which we wrap up
+    During a parse we need to maintain some state, which we wrap up
     in a value of the following 'ParserState' type. We construct the
     initial version of this state when we start parsing a document
     and, when done, extract any relevant information from it before we
     throw it away.
 
-    The type system will make sure that values of this state exist
-    and are handled only during a specific parse run and cannot leak
-    out to the rest of the program.
+    The type system will make sure that values of this state exist and
+    are handled only during a specific parse run; they cannot leak out
+    to the rest of the program.
 -}
 data ParserState = PState
     { input :: String       -- ^Text remaining to be parsed
@@ -43,48 +44,50 @@ data ParserState = PState
 
 {-  |
     A "Parser of a" is a function that may be run during a parse and
-    will give back a value of type "a". (Frequently it would parse
-    this value out of the input stream.) These functions may be
-    combined to produce new, more complex parsers, so they are called
-    "combinators." They can be used only within a parse run. A parser
-    that has no particular value to return will generally give back
-    '()', the sole inhabitant of type '()' (pronounced "Unit").
+    will produce a value of type "a". (Usually it would parse this
+    value out of the input stream.) These functions may be combined to
+    produce new, more complex parsers, so they are called "combinators."
+    The combinators can be used only within a parse run, and this is
+    enforced by the type system. A parser that produces "no value"
+    (i.e., is used for its side-effects only) will by convention
+    produce '()', the sole inhabitant of type '()' (both pronounced
+    "Unit").
 
-    Hidden inside a @Parser a@ is a function that takes a
-    'ParserState' and returns a tuple of @(a, ParserState)@, that is,
-    the value it wants to give back and the new, potentially updated
-    state. (This function may be extracted with the 'parse' function
-    automatically defined by our label on the field below.) So
-    internally the parser combinator has full access to use and change
-    parser state, such as by reducing the remaining input (consuming
-    input characters), though this isn't visible from the outside.
+    Stored inside a @Parser a@ is a function that takes a 'ParserState'
+    and produces a tuple @(a, ParserState)@, this being the value it
+    wants to produce and the new (usually updated) state. (This
+    function may be extracted with the 'parse' function automatically
+    defined by our label on the field below.) So internally the parser
+    combinator has full access to use and change parser state, such as
+    by reducing the remaining input (consuming input characters), but
+    this isn't visible from the outside.
 
-    The whole point behind wrapping functions up in this is to
-    separate the "pure" parts of a parsing function (such as
-    converting a string to a number) from the other details, such as
-    the current parser state and selection and order of parser
+    The whole point behind wrapping functions up in this data
+    structure is to separate the "pure" parts of a parsing function
+    (such as converting a string to a number) from the other details,
+    such as the current parser state and selection and order of parser
     functions, which we call the "structure" of a Parser. (This is
     probably the most difficult part of a monadic parser to
-    understand.) We will later see the split between the functions
-    that deal with parsing and the other functions that deal with
-    these structural issues.
+    understand.) We will later see how we split up the functions that
+    deal with parsing and the other functions that deal with these
+    structural issues.
 -}
 newtype Parser a = Parser { parse :: ParserState -> (a, ParserState) }
 
 {-  |
     This starts and completes a parse run by running a (top-level)
-    'Parser a' on the given input, returning a value of type 'a'
-    (typically the fully parsed AST) that it produces.
+    'Parser a' on the given input, producing the value of type 'a'
+    (typically the fully parsed AST) that the 'Parser a' produced.
 
     To do this we need to build a new 'ParserState', feed that into
     the 'parse' function contained inside the 'Parser', and handle the
-    result, which is the value of type 'a' the parser gives back and
-    the final state.
+    result, which is the value of type 'a' produced by the 'Parser a'
+    and the final state.
 
     In a more sophisticated parser framework we'd want to check the
     final state to see if there are errors, unconsumed input, or the
     like and handle that appropriately. But here we just throw away
-    the final state and return the result.
+    the final state and produce the result.
 -}
 runParser :: String     -- ^Input to be parsed
           -> Parser a   -- ^Parser to run on the input
@@ -113,7 +116,7 @@ runParser s (Parser parse) =
        'Int' @[Int]@ could be empty (@[]@), one Int (@[3]@) or three
        Ints in a specific order (@[3, 1, 2]@).
 
-    Every instance of Functor has an 'fmap' function that that takes a
+    Every instance of Functor has an 'fmap' function. `fmap` takes a
     function that can operate on the pure value(s) "contained in" the
     structure of the functorial value and produces an identical
     structure where that function has been applied to the pure values
@@ -136,11 +139,12 @@ runParser s (Parser parse) =
     'fmap' can never change the structure. 'fmap' on a 'Maybe' may
     never change a @Just x@ to a @Nothing@ or vice versa becuase the
     pure function knows nothing about the presence or absence of an
-    optional value; that's part of the structure that 'fmap never
+    optional value; that's part of the structure that 'fmap' never
     changes. Similarly, 'fmap' on a list may never change the length
-    of a list because the pure function doesn't know anything about
-    lengths; the length of a list is part of the list's structure, not
-    connected to the pure values in the list.
+    or order of a list because the pure function doesn't know anything
+    about lengths or ordering; the length and order of a list is part
+    of the list's structure, not connected to the pure values in the
+    list.
 
     This is expressed in the following laws of 'fmap'. (If you are
     mathematically inclined you can prove that any 'fmap' function
@@ -171,35 +175,35 @@ runParser s (Parser parse) =
 instance Functor Parser where
     fmap :: (a -> b) -> Parser a -> Parser b
     fmap f (Parser parse) =
-        Parser $                    -- 'fmap' gives back a new 'Parser b'
+        Parser $                    -- 'fmap' produces a new 'Parser b'
             \state ->               -- containing a function that takes a state
                 let (x, state')     -- where we get back an 'a' and new state
                       = parse state -- from running the 'Parser a'
-                 in (f x, state')   -- and put value a through 'f'
+                 in (f x, state')   -- and put the pure value through 'f'
 
 test_functor =
-     do -- Running Parser 'give42' on any input always produces 42.
-        assertEqual  42  (runParser "" give42)
-        -- Running the Parser converted to give back a String produces "42".
-        assertEqual "42" (runParser "" giveString)
+     do -- Running Parser 'theAnswer' on any input always produces Int 42.
+        assertEqual  42  (runParser "" theAnswer)
+        -- Running the Parser converted to produce a String produces "42".
+        assertEqual "42" (runParser "" theAnswerAsString)
     where
         -- | Parser that just gives the 'Int' 42, leaving the state unchanged.
-        give42 :: Parser Int
-        give42 = Parser $ \state -> (42, state)
+        theAnswer :: Parser Int
+        theAnswer = Parser $ \state -> (42, state)
         -- | Turn an 'Int' into its 'String' representation
         toString :: Int -> String
         toString x = show x
         -- | Use 'fmap' with 'toString' to convert the 'give42' parser
-        --   into one that gives a String instead of an Int
-        giveString = fmap toString give42
+        --   into one that produces a String instead of an Int
+        theAnswerAsString = fmap toString theAnswer
 
 {-  |
     The next step towards a Monad is the 'Applicative' typeclass. This
     is actually a special kind of Functor (as evidenced by the fact
     that anything that is an 'Applicative' must also be a 'Functor')
     whose full name is "applicative functor." (Or, if you want to
-    impress your friends, you can use the true mathematical name,
-    "strong lax monoidal functor.")
+    impress your friends, you can use the more math-y name, "strong lax
+    monoidal functor.")
 
     We won't speak too much here about how Applicative is used in
     programs (though some parsers work in just Applicative alone,
@@ -229,25 +233,24 @@ instance Applicative Parser where
     this go by many names.)
 
     Examples:
-    1. @pure 3@ on a @Maybe Int@ gives us @Just 3@.
-    2. @pure 3@ on a a list of Int, @[Int]@, gives us @[3]@.
+    1. @pure 3@ into @Maybe Int@ produces @Just 3@.
+    2. @pure 3@ into a list of Int, @[Int]@, produces @[3]@.
 
     This is much more restrictive than what we can do with the actual
     constructors for structure types: we cannot use 'pure' to create a
     'Nothing', an empty list, or a list of more than one value since
-    all of these involve structure as well as possibly zero or more
-    than one pure value. However, for our particular purposes here,
-    where we want to get to Monad anyway, we don't need to worry about
-    the reasons for this right now.
+    all of these involve structure and less or more than one pure
+    value. However, for our particular purposes here, where we just
+    want to get to Monad anyway, we don't need to worry about the
+    reasons for this right now.
 
     For the case of our Parser, 'pure' is quite simple. The pure value
-    is what the parser gives back and the structure is a function that
-    takes and returns a state, so we simply create a new Parser
-    containing a function that takes a state and returns our pure
+    is what the parser produces and the structure is a function that
+    takes and produces a state, so we simply create a new Parser
+    containing a function that takes a state and produces our pure
     value along with that same state. Thus, as shown by
     'test_applicative_pure', using 'runParser' on that Parser should
-    do nothing with the state and give back what was put in with
-    'pure'.
+    do nothing with the state and produce what was put in with 'pure'.
 -}
 parserPure :: a -> Parser a
 parserPure x = Parser $ \state -> (x, state)
@@ -261,23 +264,23 @@ test_applicative_pure =
     Applicative, we don't need to worry much about the '<*>' ("apply")
     function; this is just Monad's 'ap' function. However, we
     implement it here, rather than re-using `ap` as provided by Monad
-    because it does serve a useful pedagogical purpose.
+    because this serves a useful pedagogical purpose.
 
     The idea here is that apply lets you execute functions that have
-    been lifted into the structure (usually via 'pure', obviously) to
-    arguments also in the structure. Let's say we have a pure function
-    @double x = x * 2@ and a pure value @3@. In the pure world,
-    @double 3@ would give back @6@. But both the function and its
-    argument can be lifted up into the Applicative functor and be run
-    there via @pure double <*> pure 3@, giving back the same thing as
-    @pure 6@: i.e., all the structure in the Applicative functor is
-    preserved and dealt with properly by 'pure' and '<*>'.
+    been lifted into the structure (e.g. via 'pure') to arguments also
+    in the structure. Let's say we have a pure function @double x = x
+    * 2@ and a pure value @3@. In the pure world, @double 3@ would
+    produce @6@. But both the function and its argument can be lifted
+    up into the Applicative functor and be run there via @pure double
+    <*> pure 3@, giving back the same thing as @pure (double 3)@:
+    i.e., all the structure in the Applicative functor is preserved
+    and dealt with properly by 'pure' and '<*>'.
 
-    So while here we don't care why we need '<*>', the "how" is worth
-    understanding.
+    So while here we don't care why we need '<*>', how it works is
+    worth understanding.
 
     Our Parser's structure around pure values so far has been "a
-    function that takes a state and returns the new state plus the
+    function that takes a state and produces the new state plus the
     pure value." But we actually want more structure than that (this
     is why we're not just a Functor): we want also to /chain/ the
     state through sequential applications of the functions in the
@@ -289,9 +292,10 @@ test_applicative_pure =
 
     The '<*>' operator is the first time so far we've seen explicit
     sequencing of two Parsers, and our implementation of '<*>'
-    reflects that. We produce a new Parser containing a parse function
-    that, in the usual way, accepts a state and returns a new state
-    plus a result value. Its sequence of operations is:
+    reflects that. Our implementation of '<*>' produces a new Parser
+    containing a parse function that, in the usual way, accepts a
+    state and produces a new state plus a result value. The sequence
+    of operations is:
     1. Via pattern matching, extract the two parse functions,
        @parse_f@ and @parse_x@ from the two Parsers we've been given.
     2. Apply the first parse function, @parse_f@, to our input @state@
@@ -304,15 +308,25 @@ test_applicative_pure =
     4. Apply the pure function @f@ from the first Parser to the pure
        value @x@ from the second Parser to produce a new pure result
        value.
-    5. Give back that new pure result value and the final state after
+    5. Produce that new pure result value and the final state after
        modification (if any) by the first and second Parsers.
+
+    In other words, we have two separate streams of operations here:
+    passing the state through each Parser in turn to update it, and
+    applying the pure function "contained" in the first parser to the
+    pure value "contained" in the second parser. At the end, we wrap
+    up the results of these two streams together for further
+    processing.
 
     'test_applicative_apply' below demonstrates two uses of this. The
     first applies a function of a single argument ("double") to a
     single value. The second, in the typical Haskell way, partially
-    applies a function of two arguments ("multiply") to the first
-    value to produce a function of a single argument which, applied to
-    the second values, produces the result.
+    applies a function taking two arguments ("multiply") to the first
+    value to produce a function taking a single argument which,
+    applied to the second values, produces the result. (Technically,
+    in Haskell a function that takes two values is actually a function
+    that takes one value and produces another function that takes one
+    value. This is called "partial application.")
 -}
 parserApply :: Parser (a -> b) -> Parser a -> Parser b
 (Parser parse_f) `parserApply` (Parser parse_x) =
@@ -327,8 +341,12 @@ test_applicative_apply =
         --  Lift multiply and partially apply to 3, then apply to 5.
         assertEqual 15 (runParser "" $ pure (*) <*> pure 3 <*> pure 5)
 
-{- Actually, you can use a slightly simpler, but perhaps not quite
-   as clear definition, too:
+{- You may have noticed that extracting the pure value from a Functor,
+   applying a pure function to it, and putting the pure result back
+   into a Functor is something we've already written before: 'fmap'.
+   If so, good for you! We could actually re-use this code in our
+   definition above:
+
 parserApply :: Parser (a -> b) -> Parser a -> Parser b
 (Parser parse_f) `parserApply` xparser =
     Parser $ \state ->
@@ -341,8 +359,8 @@ parserApply :: Parser (a -> b) -> Parser a -> Parser b
     our Parser means sequencing the bits of the input that we read to
     produce a final result.
 
-    Again, we need two functions: 'return' and '>>=' (pronounced
-    "bind").
+    Again, we need two functions: 'return' (not the same thing as
+    @return@ in other languages!) and '>>=' (pronounced "bind").
 
     The 'return' function lifts pure values into the Monad structure.
     If this sounds familiar it's because we've already done this
